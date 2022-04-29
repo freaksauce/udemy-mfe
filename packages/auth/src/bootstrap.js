e3b38e996e2144f1b64bec7bdb77770d72234bfa
Jon <jon.bloomer@iag.com.au>
2022-04-29 16:30:36 +1000

    lots of MFE updates

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -25,8 +25,10 @@ const mount = (el, { onNavigate, defaultHistory }) => {
 }
 
 // If in dev and in isolation call mount immediately
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root')
+  const devRoot = document.querySelector('#_auth-dev-root')
+  console.log('devRoot: ', devRoot);
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() })
   }
