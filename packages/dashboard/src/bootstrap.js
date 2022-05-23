a26152c3d08b807e55a8bba6b3a64e86ae44b509
Jon <jon.bloomer@iag.com.au>
2022-05-23 16:23:32 +1000

    added Dashboard

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import Dashboard from './components/Dashboard.vue'
+
+// Mount function to start app
+const mount = (el) => {
+  const app = createApp(Dashboard)
+  app.mount(el)
+}
+
+// If in dev and in isolation call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_dashboard-dev-root')
+  if (devRoot) {
+    mount(devRoot)
+  }
+}
+
+// If running through container export the mount function
+export { mount }
